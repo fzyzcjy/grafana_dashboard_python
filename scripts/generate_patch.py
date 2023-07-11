998cc084e98206ed6a69e561a6d580f62ddab513
fzyzcjy <ch271828n@outlook.com>
2023-07-11 13:28:27 +0800

    generate_patch.py

---
+++ scripts/generate_patch.py
@@ -0,0 +1,12 @@
+import difflib
+from pathlib import Path
+
+dir_before = Path(__file__).parents[1] / 'misc/model_generation/raw_generated'
+dir_after = Path(__file__).parents[1] / 'grafana_dashboard/model'
+dir_diff = Path(__file__).parents[1] / 'misc/model_generation/manual_patch'
+
+for path_before in dir_before.glob('*.py'):
+    path_after = dir_after / path_before.name
+    delta = difflib.unified_diff(path_before.read_text(), path_after.read_text(), path_before.name, path_after.name)
+    delta_text = '\n'.join(delta)
+    (dir_diff / path_before.name).write_text(delta_text)
