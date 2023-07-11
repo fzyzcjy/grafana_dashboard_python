20d21c459329649dd592ef8698958cba0bf91db1
fzyzcjy <ch271828n@outlook.com>
2023-07-11 10:54:42 +0800

    try manually edit schema and gen

--- scripts/generate_pydantic_model.sh
+++ scripts/generate_pydantic_model.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+# --input ../third_party/grok/jsonschema/v10.0.0/kinds/core/dashboard/x/dashboard_types_gen.json \
 datamodel-codegen \
-  --input ../third_party/grok/jsonschema/v10.0.0/kinds/core/dashboard/x/dashboard_types_gen.json \
+  --input a.yaml \
   --input-file-type openapi \
-  --output ../grafana_dashboard/models/dashboard_types_gen/
+  --output ../grafana_dashboard/models/dashboard_types_gen.py
