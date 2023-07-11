6c8e6e4611805641b9d1d5c38792407ac46708e4
fzyzcjy <ch271828n@outlook.com>
2023-07-11 16:07:08 +0800

    auto fill defaults.custom

--- grafana_dashboard/manual_models.py
+++ grafana_dashboard/manual_models.py
@@ -1,12 +1,27 @@
-from typing import Literal
+from typing import Literal, Dict, List
 
 from grafana_dashboard.model import piechartpanelcfg_types_gen, timeseriespanelcfg_types_gen, tablepanelcfg_types_gen
 from grafana_dashboard.model.dashboard_types_gen import Panel
 
 
-class TimeSeries(Panel):
+class _DefaultPanel(Panel):
+    def __init__(self, **kwargs):
+        _dict_nested_fill_default_value(
+            kwargs, ['fieldConfig', 'defaults', 'custom'], self.__class__._default_field_config_custom())
+        super().__init__(**kwargs)
+
+    @classmethod
+    def _default_field_config_custom(cls):
+        raise NotImplementedError
+
+
+class TimeSeries(_DefaultPanel):
     type: Literal['timeseries'] = 'timeseries'
     options: timeseriespanelcfg_types_gen.PanelOptions = timeseriespanelcfg_types_gen.PanelOptions()
+
+    @classmethod
+    def _default_field_config_custom(cls):
+        return timeseriespanelcfg_types_gen.GraphFieldConfig()
 
 
 class PieChart(Panel):
@@ -17,3 +32,13 @@ class PieChart(Panel):
 class Table(Panel):
     type: Literal['table'] = 'table'
     options: tablepanelcfg_types_gen.PanelOptions = tablepanelcfg_types_gen.PanelOptions()
+
+
+def _dict_nested_fill_default_value(d: Dict, keys: List[str], value):
+    if len(keys) == 1:
+        if keys[0] not in d:
+            d[keys[0]] = value
+    else:
+        if keys[0] not in d:
+            d[keys[0]] = {}
+        _dict_nested_fill_default_value(d[keys[0]], keys[1:], value)
