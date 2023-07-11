c39ae37cc3f752d783982828f5c10ff8cd549777
fzyzcjy <ch271828n@outlook.com>
2023-07-11 11:35:21 +0800

    try roundtrip

--- grafana_dashboard/simple_experiment.py
+++ grafana_dashboard/simple_experiment.py
@@ -1,4 +1,4 @@
-from grafana_dashboard.generated.dashboard_types_gen import Spec
+from grafana_dashboard.generated.dashboard_types_gen import *
 
 # experiment, just create one from GUI and copy it here
 sample_json = {
@@ -149,3 +149,41 @@ sample_json = {
 
 sample_dashboard = Spec.parse_obj(sample_json)
 print(sample_dashboard)
+print(repr(sample_dashboard))
+
+copied_repr_of_sample_dashboard = Spec(id=None, uid=None, title='New dashboard', description=None, revision=None,
+                                       gnetId=None, tags=[], style=Style.dark, timezone='', editable=True,
+                                       graphTooltip=DashboardCursorSync.integer_0,
+                                       # NOTE HACK `Time`
+                                       time=Time.parse_obj({'from': 'now-6h', 'to': 'now'}),
+                                       timepicker=None, fiscalYearStartMonth=0, liveNow=False, weekStart='',
+                                       refresh=None, schemaVersion=37, version=0, panels=[
+        Panel(type='timeseries', id=2, pluginVersion=None, tags=None, targets=[Target()], title='Panel Title',
+              description=None, transparent=False, datasource=Datasource(type='prometheus', uid='PBFA97CFB590B2093'),
+              gridPos=GridPos(h=9, w=12, x=0, y=0, static=None), links=None, repeat=None,
+              repeatDirection=RepeatDirection.h, repeatPanelId=None, maxDataPoints=None, thresholds=None,
+              timeRegions=None, transformations=[], interval=None, timeFrom=None, timeShift=None, libraryPanel=None,
+              options={'legend': {'calcs': [], 'displayMode': 'list', 'placement': 'bottom', 'showLegend': True},
+                       'tooltip': {'mode': 'single', 'sort': 'none'}}, fieldConfig=FieldConfigSource(
+                defaults=FieldConfig(displayName=None, displayNameFromDS=None, description=None, path=None,
+                                     writeable=None, filterable=None, unit=None, decimals=None, min=None, max=None,
+                                     mappings=[], thresholds=ThresholdsConfig(mode=ThresholdsMode.absolute, steps=[
+                        Threshold(value=None, color='green', index=None, state=None),
+                        Threshold(value=80.0, color='red', index=None, state=None)]),
+                                     color=FieldColor(mode=ModeEnum.palette_classic, fixedColor=None, seriesBy=None),
+                                     links=None, noValue=None,
+                                     custom={'axisCenteredZero': False, 'axisColorMode': 'text', 'axisLabel': '',
+                                             'axisPlacement': 'auto', 'barAlignment': 0, 'drawStyle': 'line',
+                                             'fillOpacity': 0, 'gradientMode': 'none',
+                                             'hideFrom': {'legend': False, 'tooltip': False, 'viz': False},
+                                             'lineInterpolation': 'linear', 'lineWidth': 1, 'pointSize': 5,
+                                             'scaleDistribution': {'type': 'linear'}, 'showPoints': 'auto',
+                                             'spanNulls': False, 'stacking': {'group': 'A', 'mode': 'none'},
+                                             'thresholdsStyle': {'mode': 'off'}}), overrides=[]))],
+                                       templating=Templating(list=[]), annotations=AnnotationContainer(list=[
+        AnnotationQuery(name='Annotations & Alerts', datasource=Datasource(type='grafana', uid='-- Grafana --'),
+                        enable=True, hide=True, iconColor='rgba(0, 211, 255, 1)', filter=None,
+                        target=AnnotationTarget(limit=100, matchAny=False, tags=[], type='dashboard'),
+                        type='dashboard')]), links=[], snapshot=None)
+
+print(sample_dashboard == copied_repr_of_sample_dashboard)
