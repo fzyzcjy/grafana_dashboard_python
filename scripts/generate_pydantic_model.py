b4fc64fa65e64732bcd3c88ccc4a56f00924955d
fzyzcjy <ch271828n@outlook.com>
2023-07-11 11:19:36 +0800

    try to transform

--- scripts/generate_pydantic_model.py
+++ scripts/generate_pydantic_model.py
@@ -22,8 +22,35 @@ def _ensure_python_package(d: Path):
     (d / '__init__.py').write_text('')
 
 
-def _transform_openapi_schema(raw: Dict) -> Dict:
-    return TODO
+def _dict_rename_key(d: Dict, key_old: str, key_new: str):
+    assert key_new != key_old
+    d[key_new] = d[key_old]
+    del d[key_old]
+
+
+def _sanitize_schema_key(raw_key: str) -> str:
+    return raw_key.replace('.', '_').replace('#', '')
+
+
+def _transform_openapi_schema(raw_str: str) -> str:
+    primary_dict = json.loads(raw_str)
+    schemas = primary_dict['components']['schemas']
+
+    interest_schema_keys = [
+        schema_key for schema_key in schemas
+        if _sanitize_schema_key(schema_key) != schema_key
+    ]
+
+    for schema_key in interest_schema_keys:
+        _dict_rename_key(schemas, schema_key, _sanitize_schema_key(schema_key))
+
+    transformed_str = json.dumps(primary_dict)
+
+    for schema_key in interest_schema_keys:
+        prefix = '#/components/schemas/'
+        transformed_str = transformed_str.replace(prefix + schema_key, prefix + _sanitize_schema_key(schema_key))
+
+    return transformed_str
 
 
 dir_src = Path(__file__).parents[1] / 'third_party/grok/jsonschema/v10.0.0/kinds'
@@ -41,7 +68,7 @@ for p_src in dir_src.glob('**/*.json'):
     p_target = dir_target / f'{p_src.stem}.py'
     print(f'Handle: {p_src} -> {p_target}')
 
-    p_temp.write_text(json.dumps(_transform_openapi_schema(json.loads(p_src.read_text()))))
+    p_temp.write_text(_transform_openapi_schema(p_src.read_text()))
 
     _execute_datamodel_codegen(
         path_input=p_temp,
