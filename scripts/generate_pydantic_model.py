80e2d74fd04433f68ca06acbb38e418bde3de066
fzyzcjy <ch271828n@outlook.com>
2023-07-11 10:59:52 +0800

    avoid generating time

--- scripts/generate_pydantic_model.py
+++ scripts/generate_pydantic_model.py
@@ -6,7 +6,8 @@ def _execute_datamodel_codegen():
         'datamodel-codegen '
         '--input a.yaml '
         '--input-file-type openapi '
-        '--output ../grafana_dashboard/models/dashboard_types_gen.py'
+        '--output ../grafana_dashboard/models/dashboard_types_gen.py '
+        "--custom-file-header '# AUTO GENERATED, PLEASE DO NOT MODIFY BY HAND'"
     )
 
 
