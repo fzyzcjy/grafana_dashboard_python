52da870b5c52873c568bc9143e92516df1b0e103
fzyzcjy <ch271828n@outlook.com>
2023-07-11 13:30:28 +0800

    fix

--- scripts/generate_patch.py
+++ scripts/generate_patch.py
@@ -7,6 +7,10 @@ dir_diff = Path(__file__).parents[1] / 'misc/model_generation/manual_patch'
 
 for path_before in dir_before.glob('*.py'):
     path_after = dir_after / path_before.name
-    delta = difflib.unified_diff(path_before.read_text(), path_after.read_text(), path_before.name, path_after.name)
+    delta = difflib.unified_diff(
+        path_before.read_text().split('\n'), path_after.read_text().split('\n'),
+        path_before.name, path_after.name,
+        lineterm='',
+    )
     delta_text = '\n'.join(delta)
     (dir_diff / path_before.name).write_text(delta_text)
