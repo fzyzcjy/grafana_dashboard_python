065aafd4e0e71bd84452f15a996e5de59e0c65b5
fzyzcjy <ch271828n@outlook.com>
2023-07-11 13:48:42 +0800

    fix: should not be recursive to avoid pycache folder

--- scripts/generate_patch.sh
+++ scripts/generate_patch.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-diff -Naur ../misc/model_generation/raw_generated/ ../grafana_dashboard/model/ > ../misc/model_generation/manual_patch.patch
+diff -Nau ../misc/model_generation/raw_generated/ ../grafana_dashboard/model/ > ../misc/model_generation/manual_patch.patch
