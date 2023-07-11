d2f9380807163126c3e86f7e03811888e4e51653
fzyzcjy <ch271828n@outlook.com>
2023-07-11 10:50:42 +0800

    fix: type is openapi

--- scripts/generate_pydantic_model.sh
+++ scripts/generate_pydantic_model.sh
@@ -2,5 +2,5 @@
 
 datamodel-codegen \
   --input ../third_party/grok/jsonschema/v10.0.0/kinds/core/dashboard/x/dashboard_types_gen.json \
-  --input-file-type jsonschema \
+  --input-file-type openapi \
   --output ../grafana_dashboard/models/dashboard_types_gen.py
