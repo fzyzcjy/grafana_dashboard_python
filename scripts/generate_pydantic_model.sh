689c5cd2f3d465ca992bd2a91b879bdd956f8ce5
fzyzcjy <ch271828n@outlook.com>
2023-07-11 10:45:48 +0800

    add generate script #10111

---
+++ scripts/generate_pydantic_model.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+datamodel-codegen \
+  --input ../third_party/grok/jsonschema/v10.0.0/kinds/core/dashboard/x/dashboard_types_gen.json \
+  --input-file-type jsonschema \
+  --output ../grafana_dashboard/models/dashboard_types_gen.py
