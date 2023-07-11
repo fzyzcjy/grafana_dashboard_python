8c6f08c3782dae6c7a43572aa8635415f10c6420
fzyzcjy <ch271828n@outlook.com>
2023-07-11 10:52:51 +0800

    output dir

--- scripts/generate_pydantic_model.sh
+++ scripts/generate_pydantic_model.sh
@@ -3,4 +3,4 @@
 datamodel-codegen \
   --input ../third_party/grok/jsonschema/v10.0.0/kinds/core/dashboard/x/dashboard_types_gen.json \
   --input-file-type openapi \
-  --output ../grafana_dashboard/models/dashboard_types_gen.py
+  --output ../grafana_dashboard/models/dashboard_types_gen/
