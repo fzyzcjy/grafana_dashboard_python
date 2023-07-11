00fdbad6e8977e3db0c1277042e45fd83b6be85b
fzyzcjy <ch271828n@outlook.com>
2023-07-11 14:09:07 +0800

    fix enum repr #10118

--- grafana_dashboard/utils.py
+++ grafana_dashboard/utils.py
@@ -1,5 +1,26 @@
+from dataclasses import dataclass
+from enum import Enum
+
 from pydantic import BaseModel
 
 
 class MyBaseModel(BaseModel):
-    pass
+    def __repr_args__(self):
+        return [
+            (k, _transform_repr_arg_value(v))
+            for k, v in super().__repr_args__()
+        ]
+
+
+def _transform_repr_arg_value(v):
+    if isinstance(v, Enum):
+        return _Reprable(str(v))
+    return v
+
+
+@dataclass
+class _Reprable:
+    content: str
+
+    def __repr__(self):
+        return self.content
