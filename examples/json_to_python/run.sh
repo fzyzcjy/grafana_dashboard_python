a07f095cdf815ed9c9546d1027e204f2a0b30fe6
fzyzcjy <ch271828n@outlook.com>
2023-07-12 09:19:47 +0800

    let example use published pkg

--- examples/json_to_python/run.sh
+++ examples/json_to_python/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# use `grafana_dashboard` command directly if you have the package installed
-(cd ../.. && python -m grafana_dashboard.grafana_dashboard json-to-python \
+set -eux
+
+# If you want to test source code instead of published package:
+# `python -m grafana_dashboard.grafana_dashboard`
+(cd ../.. && grafana_dashboard json-to-python \
   --json-path examples/json_to_python/input.json \
   --python-path examples/json_to_python/output.py
 )
--- examples/python_to_json/run.sh
+++ examples/python_to_json/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# use `grafana_dashboard` command directly if you have the package installed
-(cd ../.. && python -m grafana_dashboard.grafana_dashboard python-to-json \
+set -eux
+
+# If you want to test source code instead of published package:
+# `python -m grafana_dashboard.grafana_dashboard`
+(cd ../.. && grafana_dashboard python-to-json \
   --python-base-dir examples/python_to_json \
   --python-base-package input_python \
   --json-dir examples/python_to_json/output_json
