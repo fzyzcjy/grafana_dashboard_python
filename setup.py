26bce85af7a76bd098f9edbcdf5141684534be3a
fzyzcjy <ch271828n@outlook.com>
2023-07-12 09:04:32 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import find_packages, setup
+
+
+# reference: https://github.com/thu-ml/tianshou/blob/master/setup.py
+
+def get_version() -> str:
+    # https://packaging.python.org/guides/single-sourcing-package-version/
+    init = open(os.path.join("grafana_dashboard", "__init__.py"), "r").read().split()
+    return init[init.index("__version__") + 2][1:-1]
+
+
+def get_install_requires():
+    return [
+        "pydantic>=1.10.2",
+        "dataclasses>=0.8",
+        "rich>=13.4.2",
+        "typer>=0.6.1",
+    ]
+
+
+setup(
+    name="grafana_dashboard",
+    version=get_version(),
+    description="Write Grafana dashboards in Python, without losing thousands of dashboards in the zoo",
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/fzyzcjy/grafana-dashboard-python",
+    author="fzyzcjy",
+    license="MIT",
+    python_requires=">=3.6",
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="grafana dashboard json",
+    packages=find_packages(
+        exclude=["test", "test.*", "examples", "examples.*", "docs", "docs.*"]
+    ),
+    install_requires=get_install_requires(),
+)
